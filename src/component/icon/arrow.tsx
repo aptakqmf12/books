3d0529b32fe71d7f69f0340476f198474ad63508
김태완 <aptakqmf12@gmail.com>
2024-03-08 11:40:29 +0900

    icon 추가

--- src/component/icon/arrow.tsx
+++ src/component/icon/arrow.tsx
@@ -5,7 +5,7 @@ export default function ArrowIcon({
   height = 8,
   fill = "#B1B8C0",
   direction = "bottom",
-}: IconProps & { direction: "up" | "bottom" }) {
+}: IconProps & { direction: "up" | "bottom" | "right" | "left" }) {
   switch (direction) {
     case "bottom":
       return (
@@ -34,6 +34,36 @@ export default function ArrowIcon({
         >
           <path
             d="M12 8L7 3L2 8L1.19249e-08 7L7 8.34742e-08L14 7L12 8Z"
+            fill={fill}
+          />
+        </svg>
+      );
+    case "left":
+      return (
+        <svg
+          width={width}
+          height={height}
+          viewBox="0 0 6 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 9.42857L3.75 5.5L0 1.57143L0.75 0L6 5.5L0.75 11L0 9.42857Z"
+            fill={fill}
+          />
+        </svg>
+      );
+    case "right":
+      return (
+        <svg
+          width={width}
+          height={height}
+          viewBox="0 0 6 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6 1.57143L2.25 5.5L6 9.42857L5.25 11L-5.22165e-07 5.5L5.25 8.80614e-07L6 1.57143Z"
             fill={fill}
           />
         </svg>
